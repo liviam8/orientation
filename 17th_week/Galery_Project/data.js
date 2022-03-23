227f7a209e6f4418203eed90f38998a56b7569a3
liviam8 <48126141+liviam8@users.noreply.github.com>
2022-03-23 10:59:25 +0100

    Beginning
    Beginning of a Galley Project

---
+++ 17th_week/Galery_Project/data.js
@@ -0,0 +1,16 @@
+const images = [
+    {
+        tilte: "Kitten",
+        description: "asjjabfnlab",
+        file: "cat.jpg"
+
+    },
+    {
+        tilte: "Kitten",
+        description: "asjjabfnlab",
+        file: "https://placekitten.com/1001/701"
+
+    }
+]
+
+// this file is for storing data about your images, you can easily access everything thats needed for your js file
---
+++ 17th_week/Galery_Project/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+    // this will run, when every content on the page is loaded
+    //function for arrows
+    //function for thumbanail render
+    //...  
+})
+
+// window.onload does the same 
